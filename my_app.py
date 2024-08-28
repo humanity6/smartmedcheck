8a0dd7510b5a44f62e9f38510da8b82f71d5e667
Arshad Faisal <54690764+humanity6@users.noreply.github.com>
2024-08-28 23:51:28 +0500

    Add files via upload

---
+++ my_app.py
@@ -0,0 +1,8 @@
+from appwrite.client import Client
+from appwrite.services.databases import Databases
+from appwrite.id import ID
+
+client = Client()
+client.set_endpoint('https://cloud.appwrite.io/v1')
+client.set_project('66cf699a000c8d6c5920')
+client.set_key('040152f0a1c76f19922fa1c1092c7834d8d0231405924a2fc66862078e3e368e522e07588cf75d8bce86f567debe0f94de7128c0476578f42b21c49e17cfe551f5ea6bfb903ca2ef780116ce9f4ad83a2ee7ec8ac27c4b17154982e7a573f97be600ce25e6e8d746a2f02bac1bd3eb7417c27188fac4e4fcb0d0929933b989a0')
